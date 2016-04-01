b8375ef480925956d259840e1329c56e9723f556
btctestsys <syahrunsulaiman@gmail.com>
2016-04-01 18:48:30 +0800

    Update

--- resources/views/emails/emailRegister.blade.php
+++ resources/views/emails/emailRegister.blade.php
@@ -7,7 +7,7 @@ You have submitted request to register an account with us.<br>
 <br>
 This email is a confirmation for your registration. If you think this is a mistake, please ignore the email. If not, please confirm the registration by clicking on the following link:<br>
 <br>
-http://btcpandalatest/verifyEmail/<?php echo $token; ?><br>
+{{$_ENV['HOST_NAME']}}/verifyEmail/<?php echo $token; ?><br>
 <br>
 We hope you have a pleasant experience with us and look forward to serve you in the near future.<br>
 <br>
